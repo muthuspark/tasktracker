1c3c5fc33e87a63d70aebf0dd27fe9a41433a708
Muthukrishnan <muthukrishnan749@gmail.com>
2015-02-04 10:57:25 +0530

    Removed port 3000

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+//app.set('view engine', 'jade');
 
 app.use(favicon());
 app.use(logger('dev'));
@@ -51,5 +51,5 @@ app.use(function(err, req, res, next) {
         error: {}
     });
 });
-app.listen(3000);
+
 module.exports = app;
